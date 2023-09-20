f91802155c2a20e1b6f546cb745f7996282d5c8e
Emma Fu <emmafu301@hotmail.com>
2023-09-20 17:37:43 -0400

    Activity 4: Completed

--- activity4.py
+++ activity4.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+
 # Activity 4
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -15,7 +16,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    # email = StringField('What is your UofT Email address?', validators=[DataRequired(), Email()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 
@@ -27,9 +28,15 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['email'] = form.email.data
+
         return redirect(url_for('index'))
 
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 
 @app.route('/user/<name>')
