9a6d94baaebaeee9a816b3feadffa01af51a0b5f
Emma Fu <emmafu301@hotmail.com>
2023-09-20 17:10:57 -0400

    Activity 4: example 4-4

--- activity4.py
+++ activity4.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 # Activity 4
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'very hard to guess string'
@@ -15,12 +15,18 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    # email = StringField('What is your UofT Email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''  # If doesn't include this line, the input box will show the input given by user
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>')
